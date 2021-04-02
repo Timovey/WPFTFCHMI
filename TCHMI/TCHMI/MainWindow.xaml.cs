41ab36c3b4fc010f93d8aa04e81c54f2e8bcf137
Timofey Kochetkov <pochta30052001@gmail.com>
2021-04-02 21:50:14 +0400

    add project

---
+++ TCHMI/TCHMI/MainWindow.xaml.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TFCHMI
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private TimeSpan timespan = TimeSpan.MinValue;
+        private bool isPause = false;
+        private bool isPlay = false;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void ButtonRunAway_Click(object sender, RoutedEventArgs e)
+        {
+           // media.Source = new Uri("Media\\Gandalf.mp4", UriKind.Relative);
+            media.Position = TimeSpan.Zero;
+            if (media.Source != null)
+            {
+                media.Play();
+                isPlay = true;
+            }
+            else
+            {
+                throw new Exception("Видео не найдено");
+            }
+        }
+        private void ButtonShrek_Click(object sender, RoutedEventArgs e)
+        {
+           //  media.Source = new Uri("Media\\Shrek.mp4", UriKind.Relative);
+            media.Position = TimeSpan.Zero;
+            if (media.Source != null)
+            {
+                media.Play();
+                isPlay = true;
+            }
+            else
+            {
+                throw new Exception("Видео не найдено");
+            }
+        }
+        private void ButtonVolk_Click(object sender, RoutedEventArgs e)
+        {
+          // media.Source = new Uri("Media\\Volk.MP4", UriKind.Relative);
+            media.Position = TimeSpan.Zero;
+            if (media.Source != null)
+            {
+                media.Play();
+                isPlay = true;
+            }
+            else
+            {
+                throw new Exception("Видео не найдено");
+            }
+        }
+
+        private void ButtonStop_Click(object sender, RoutedEventArgs e)
+        {
+            media.Close();
+            media.Source = null;
+            timespan = TimeSpan.MinValue;
+            isPlay = false;
+        }
+        private void ButtonPause_Click(object sender, RoutedEventArgs e)
+        {
+            if (media.Source != null && !isPause)
+            {
+                isPause = true;
+                isPlay = false;
+                timespan = media.Position;
+                media.Stop();
+            }
+
+        }
+        private void butPlay_Click(object sender, RoutedEventArgs e)
+        {
+            if (media.Source != null && !isPlay)
+            {
+                if(timespan != TimeSpan.MinValue)
+                {
+                    media.Position = timespan;
+                }
+                isPlay = true;
+                isPause = false;
+                media.Play();
+            }
+        }
+
+        private void volume_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (media != null && media.Source != null)
+            {
+                media.Volume = e.NewValue / 100;
+            }
+        }
+    }
+}
